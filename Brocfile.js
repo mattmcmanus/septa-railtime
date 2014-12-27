2a103bf3cc620e79fe9960820ff55bd9fcabfd33
Matt McManus <matt@mcmanus.io>
2014-12-27 15:41:27 -0500

    es6 support

--- Brocfile.js
+++ Brocfile.js
@@ -2,8 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
 var app = new EmberApp({
+  '6to5': {
+    // disable comments
+    comments: false
+  },
   sassOptions: {
     includePaths: [
       'bower_components/bourbon/dist'
