4d085735b6c74b4ddf06b083c9406ca55c786f87
Matt McManus <matt@mcmanus.io>
2015-01-09 21:42:34 -0500

    Trains display

--- app/models/train.js
+++ app/models/train.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  number: DS.attr('number'),
   line: DS.attr('string'),
   departureTime: DS.attr('string'),
   arrivalTime: DS.attr('string'),
